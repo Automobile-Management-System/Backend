87fe5ebee0ab2d118d58d90223c16baaddee6b5c
eshancool123 <eshansenadhi5@gmail.com>
2025-10-10 02:52:26 +0530

    Add ServiceAnalyticsRepository with GetServicesAsync method
    A new `ServiceAnalyticsRepository` class has been introduced, implementing the `IServiceAnalyticsRepository` interface. This class is located in the `automobile_backend.Repository` namespace and interacts with the database using a private readonly `ApplicationDbContext` field.
    
    The class includes an asynchronous method, `GetServicesAsync`, which retrieves a list of `Service` entities from the database using Entity Framework's `ToListAsync`. Necessary namespaces have been imported to support this functionality.

---
+++ Repository/ServiceAnalyticsRepository.cs
@@ -0,0 +1,23 @@
+using automobile_backend.InterFaces.IRepository;
+using automobile_backend.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace automobile_backend.Repository
+{
+    public class ServiceAnalyticsRepository : IServiceAnalyticsRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ServiceAnalyticsRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Service>> GetServicesAsync()
+        {
+            return await _context.Services.ToListAsync();
+        }
+    }
+}
