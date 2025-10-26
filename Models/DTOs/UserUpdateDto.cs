b232263d71473b49dc288959505490107fa0f772
JanithAberathna <dilshanjanith68@gmail.com>
2025-10-26 23:07:01 +0530

    User Management Dtos

---
+++ Models/DTOs/UserUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿namespace automobile_backend.Models.DTOs
+{
+    public class UserUpdateDto
+    {
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? Status { get; set; }
+    }
+}
