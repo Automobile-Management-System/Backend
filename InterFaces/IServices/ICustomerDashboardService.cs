1e5421b3f058e210452fe871ff33e39016b3d5d3
JanithAberathna <dilshanjanith68@gmail.com>
2025-10-27 23:15:33 +0530

    Customer dashboard

--- InterFaces/IServices/ICustomerDashboardService.cs
+++ InterFaces/IServices/ICustomerDashboardService.cs
@@ -1,6 +1,7 @@
-﻿using automobile_backend.Models.DTOs;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using automobile_backend.Models.DTOs;
+using automobile_backend.Models.Entities;
 
 namespace automobile_backend.Interfaces.IServices
 {
@@ -8,5 +9,12 @@ namespace automobile_backend.Interfaces.IServices
     {
         Task AddVehicleAsync(int userId, CustomerVehicleDto dto);
         Task<List<CustomerVehicleDto>> GetVehiclesByUserIdAsync(int userId);
+
+        Task<int> GetUpcomingAppointmentsCountAsync(int userId);
+        Task<int> GetInProgressAppointmentsCountAsync(int userId);
+        Task<int> GetCompletedAppointmentsCountAsync(int userId);
+        Task<decimal> GetPendingPaymentsTotalAsync(int userId);
+        Task<IEnumerable<Appointment>> GetLatestServicesAsync(int userId);
+        Task<IEnumerable<Appointment>> GetLatestModificationsAsync(int userId);
     }
 }
