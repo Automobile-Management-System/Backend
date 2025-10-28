975b0921bba48816e3153ba0881f9aefea65c11b
JanithAberathna <dilshanjanith68@gmail.com>
2025-10-28 23:20:07 +0530

    backend for display services

---
+++ Controllers/ViewServiceController.cs
@@ -0,0 +1,27 @@
+﻿using automobile_backend.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace automobile_backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ViewServiceController : ControllerBase
+    {
+        private readonly IViewServiceService _viewServiceService;
+
+        public ViewServiceController(IViewServiceService viewServiceService)
+        {
+            _viewServiceService = viewServiceService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllViewServices(
+            [FromQuery] int pageNumber = 1,
+            [FromQuery] int pageSize = 12,
+            [FromQuery] string? search = null)
+        {
+            var pagedServices = await _viewServiceService.GetAllViewServicesAsync(pageNumber, pageSize, search);
+            return Ok(pagedServices);
+        }
+    }
+}
---
+++ InterFaces/IServices/IViewServiceService.cs
@@ -0,0 +1,9 @@
+﻿using automobile_backend.Models.DTOs;
+
+namespace automobile_backend.Services.Interfaces
+{
+    public interface IViewServiceService
+    {
+        Task<PagedResult<ViewServiceDto>> GetAllViewServicesAsync(int pageNumber, int pageSize, string? searchTerm);
+    }
+}
