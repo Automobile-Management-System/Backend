12420397366c35f1f38c157f14755414d1249cf6
AnjanaNimesh <anjananimesh22@gmail.com>
2025-10-28 20:06:39 +0530

    add IemployeeDashboardSerivice

---
+++ InterFaces/IServices/IEmployeeDashboardService.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace automobile_backend.InterFaces.IServices
+{
+    public interface IEmployeeDashboardService
+    {
+        Task<int> GetTodayUpcomingAppointmentCountAsync(int employeeId);
+
+        Task<int> GetInProgressAppointmentCountAsync(int employeeId);
+
+        Task<List<object>> GetTodayRecentServicesAsync(int employeeId);
+
+        Task<List<object>> GetTodayRecentModificationsAsync(int employeeId);
+
+    }
+}
