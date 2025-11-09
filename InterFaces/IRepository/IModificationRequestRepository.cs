5f76c02529c75d88647a52fc81c1b0656ca423bf
navo1212 <navodvi1212@gmail.com>
2025-11-09 13:28:50 +0530

    t5

--- InterFaces/IRepository/IModificationRequestRepository.cs
+++ InterFaces/IRepository/IModificationRequestRepository.cs
@@ -1,3 +1,4 @@
+using automobile_backend.Models.DTOs;
 using automobile_backend.Models.Entities;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -6,6 +7,7 @@ namespace automobile_backend.InterFaces.IRepository
 {
     public interface IModificationRequestRepository
     {
+        Task AddAsync(CreateModificationRequestDto dto);
         Task<IEnumerable<ModificationRequest>> GetAllAsync();
         Task<ModificationRequest?> GetByIdAsync(int id);
         Task<ModificationRequest> UpdateAsync(ModificationRequest request);
